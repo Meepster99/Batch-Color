c0331ee35e7fc1b2fa8cfc9900235c1b7b80b28f
Meepster99 <20229468+Meepster99@users.noreply.github.com>
2018-02-20 19:10:17 -0500

    Update example.bat

--- example.bat
+++ example.bat
@@ -1,1 +1,14 @@
 @echo off
+call color.bat 0a "0a"
+call color.bat 0b "0b"
+call color.bat 0c "0c"
+echo(
+echo(
+call color.bat 19 "hi"
+call color.bat 2F "black"
+call color.bat 4e "white"
+echo(
+call color.bat 0b "this is " && call color.bat 0c "on the same line" && echo(
+
+:: call color.bat 0b ">" -- this does not work
+:: restricted characters:   " ~ # % & * ( ) { } \ : < > ? / + |
